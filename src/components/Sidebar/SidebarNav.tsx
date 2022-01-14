106f02116ca1d169384c831ecad6ace979cc5af3
felixstenian <felixstenian@gmail.com>
2022-01-14 11:42:31 -0300

    create page about

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -4,7 +4,17 @@ import { NavLink } from "./NavLink"
 
 const SidebarNav = () => {
   return (
-    <Flex bg='#11233F' w={[250, 250, 131]} minWidth={130} h='100vh' justify='space-between' align='center' flexDir='column'>
+    <Flex 
+      as='aside' 
+      bg='#11233F' 
+      w={[250, 250, 131]} 
+      minWidth={130} 
+      h='100vh' 
+      justify='space-between' 
+      align='center' 
+      flexDir='column'
+      position='fixed'
+    >
       <Stack spacing={2} align='center' mt={10} borderBottom='1px solid' borderColor='#69FFDB' w='100%'>
         <Image src='/logo_1x.png' alt='Felix Logo' />
         <Text 
@@ -33,7 +43,7 @@ const SidebarNav = () => {
 
         <Box w='100%' align='center'>
           <Divider mb={3} borderColor='rgba(105, 255, 219, 0.2)' />
-          <NavLink href='/sobre' >
+          <NavLink href='/about' >
             Sobre
           </NavLink>
         </Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,47 @@
 import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
-import { Layout } from "../components";
+import { Layout, Sidebar } from "../components";
 
 export default function Home() {
   return (
     <Layout>
-      <Box maxWidth={720} px={5} ml={10}>
-          <Text fontSize={68} lineHeight='80px' fontWeight={600}>
-            Oi, meu nome é
-            <br />
-            <Box display='flex' mt={5}>
-              <Box>
-              <Image src="/logo_1x.png" alt="Felix-Logo" position='relative' />
+      {/* <Sidebar /> */}
+      {/* <Flex w='100%' px={5} justify='center' mt={20} > */}
+        <Flex maxWidth={720} h='100vh' margin={[5, 5, '0 200px']}  flexDir='column' justify='center' >
+            <Text fontSize={68} lineHeight='80px' fontWeight={600}>
+              Oi, meu nome é
+              <br />
+              <Box display='flex' mt={5}>
+                <Box>
+                <Image src="/logo_1x.png" alt="Felix-Logo" position='relative' />
+                </Box>
+                <Text pt={4}>    
+                  elix.
+                </Text>
               </Box>
-              <Text pt={4}>    
-                elix.
-              </Text>
-            </Box>
-          </Text>
-        <Flex mt={10}>
-          <Text fontSize={24} lineHeight='28px'>
-            Sou um desenvolvedor front-end especializado em criar experiências digitais excepcionais.
-          </Text>
+            </Text>
+          <Flex mt={10}>
+            <Text fontSize={24} lineHeight='28px'>
+              Sou um desenvolvedor front-end especializado em criar experiências digitais excepcionais.
+            </Text>
+          </Flex>
+          <Button 
+            mt={20} 
+            width={200} 
+            height={50} 
+            fontWeight={300} 
+            variant='outline' 
+            borderColor='#69FFDB' 
+            color='#69FFDB'
+            _hover={{
+              bg: '#69FFDB',
+              color: '#0A1A2E',
+              transition: 'background 0.5s'
+            }}
+          >
+            CONTATO
+          </Button>
         </Flex>
-        <Button 
-          mt={20} 
-          width={200} 
-          height={50} 
-          fontWeight={300} 
-          variant='outline' 
-          borderColor='#69FFDB' 
-          color='#69FFDB'
-          _hover={{
-            bg: '#69FFDB',
-            color: '#0A1A2E',
-            transition: 'background 0.5s'
-          }}
-        >
-          CONTATO
-        </Button>
-      </Box>
+      {/* </Flex> */}
     </Layout>
   )
 }
