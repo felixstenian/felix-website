60619aa7b0c05105a59f1395c0e26e94b2a15c52
felixstenian <felixstenian@gmail.com>
2022-01-17 00:16:44 -0300

    added breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,11 +2,6 @@ import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 export default createBreakpoints({
-  // sm: '30em',
-  // md: '48em',
-  // lg: '62em',
-  // xl: '80em',
-  // '2xl': '96em',
   sm: "320px",
   md: "768px",
   lg: "960px",
