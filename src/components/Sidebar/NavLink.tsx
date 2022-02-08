3b0349824d67a7497baccf7489fd473623f52242
felixstenian <felixstenian@gmail.com>
2022-02-08 16:11:52 -0300

    update path sound && functions onMouse

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -2,7 +2,6 @@ import { ReactNode, ElementType } from 'react'
 import { Link, Text, LinkProps, Icon } from '@chakra-ui/react'
 
 import useSound from 'use-sound'
-import biteSound from '../../../public/sounds/bite.mp3'
 
 import ActiveLink from '../ActiveLink'
 
@@ -13,15 +12,15 @@ interface NavLinkProps extends LinkProps {
 }
 
 export const NavLink = ({ icon, href, children, ...rest }: NavLinkProps) => {
-  const soundUrl = biteSound
+  const soundUrl = 'sounds/bite.mp3'
   const [play, { stop }] = useSound(soundUrl, { volume: 0.5 })
 
   return (
     <ActiveLink href={href} passHref>
       <Link
         {...rest}
-        onMouseEnter={play} 
-        onMouseLeave={stop}
+        onMouseEnter={() => play()} 
+        onMouseLeave={() => stop()}
         _hover={{
           textDecoration: 'none'
         }}
